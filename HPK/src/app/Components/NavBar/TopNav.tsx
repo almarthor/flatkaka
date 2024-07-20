de7b596f2a4574af00087c42102df24ce7ab5b40
almarthor <125505908+almarthor@users.noreply.github.com>
2024-07-20 14:04:34 +0000

    topnav

--- HPK/src/app/Components/NavBar/TopNav.tsx
+++ HPK/src/app/Components/NavBar/TopNav.tsx
@@ -6,7 +6,7 @@ import Nav from "./Nav";
 export default function TopNav() {
   return (
     <>
-      <header className=" flex flex-wrap bg-gray-100 top-0 sticky z-50 md:px-44  px-14 py-5 w-full items-center justify-between border-b-2 border-red-500 ">
+      <header className=" flex flex-wrap bg-gray-300 top-0 sticky z-50 md:px-44  px-14 py-5 w-full items-center justify-between border-b-2 border-red-500 ">
         <Logooo />
         <Nav />
       </header>
